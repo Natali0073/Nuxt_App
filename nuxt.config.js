564ce82575cce17536bc62ddcafaa9737723dfd9
Mykola <mv@leobit.co>
2019-10-29 18:57:22 +0200

    Add hcap to config

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,11 @@ module.exports = {
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ],
+    script: [
+      {
+        src: 'hcap.js'
+      }
+    ]
   },
   /*
    ** Customize the progress bar color
@@ -37,6 +42,7 @@ module.exports = {
     /*
      ** Run ESLint on save
      */
+    vendor: ['hcap.js'],
     extend (config, {isDev, isClient}) {
       if (isDev && isClient) {
         config.module.rules.push({
